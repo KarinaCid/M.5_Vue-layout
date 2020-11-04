93f579fcde62815d4ba3213074a1c4ac4210b0ba
Karina Cid <memento_mori.06@hotmail.com>
2020-11-03 21:25:09 -0300

    installing vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,60 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <v-toolbar-title>
-        Edutecno 0049
-      </v-toolbar-title>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
       <v-spacer></v-spacer>
-      <v-btn text>Home</v-btn>
-      <v-btn text>Login</v-btn>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
     </v-app-bar>
-    <v-content>
-      <v-card width="400px" class="my-auto mx-auto">
-        <v-card-title>
-          <h1>Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Usuario" prepend-icon="mdi-account-circle" />
-            <v-text-field
-              :type="showPassword ? 'text' : 'password'"
-              label="Pasword"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Registro</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
-      <v-footer dark padless absolute>
-        <v-card class="flex" flat>
-          <v-card-title class="primary">
-            <strong>Get connected with us on social networks!</strong>
-          </v-card-title>
-          <v-card-text class="py-2 white--text text-center">
-            {{ new Date().getFullYear() }} - <strong>Edutecno</strong>
-          </v-card-text>
-        </v-card>
-      </v-footer>
-    </v-content>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld';
+
 export default {
-  name: "App",
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
   data: () => ({
-    showPassword: false,
+    //
   }),
 };
 </script>
